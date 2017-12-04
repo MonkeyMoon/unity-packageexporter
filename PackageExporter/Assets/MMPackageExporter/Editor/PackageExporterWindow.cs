604d141985b70edd7533515ef6828eeb129b7d34
Laurent Victorino <lvictorino@monkeymoon.net>
2017-12-04 14:52:01 +0100

    Now saving package configurations at the right place (within the plugin folder).

--- PackageExporter/Assets/MMPackageExporter/Editor/PackageExporterWindow.cs
+++ PackageExporter/Assets/MMPackageExporter/Editor/PackageExporterWindow.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+using System;
 
 namespace MM.PackageExporter
 {
     public class PackageExporterWindow : EditorWindow
     {
+        private string _plugin_path;
         private AssetInfoHolder _path_holder;
         private Vector2 _scroll_position;
         private List<PackageConfigurationSave> _saves;
@@ -21,8 +24,26 @@ namespace MM.PackageExporter
             window.RefreshContent();
         }
 
+        private void FindPluginPath()
+        {
+            string[] assets = AssetDatabase.GetAllAssetPaths();
+            foreach (string path in assets)
+            {
+                if ( Path.GetFileName(path) == "PackageExporterWindow.cs" )
+                {
+                    // Directory.GetParent * 2 because Assets/MMPackageExporter is changed into E:\WORK\package-exporter\PackageExporter\Assets\MMPackageExporter 
+                    // in case of .Parent
+                    // Yup, that's weird :S
+                    _plugin_path = Directory.GetParent(Directory.GetParent(path).ToString()).ToString();
+                    return;
+                }
+            }
+
+        }
+
         public void RefreshContent()
         {
+            FindPluginPath();
             _path_holder = new AssetInfoHolder();
             _saves = new List<PackageConfigurationSave>(Resources.FindObjectsOfTypeAll<PackageConfigurationSave>());
             for ( int i = _saves.Count - 1; i >= 0; --i )
@@ -41,33 +62,81 @@ namespace MM.PackageExporter
 
         private void ExportPackage()
         {
-            PackageConfigurationSave save = ScriptableObject.CreateInstance<PackageConfigurationSave>();
-            AssetDatabase.CreateAsset(save, "Assets/" + _save_name + ".asset");
-            save.SavePackageConfiguration(_path_holder);
-            AssetDatabase.SaveAssets();
+            string save_path = EditorUtility.SaveFilePanel("Export package", "", "", "unitypackage");
+            if (string.IsNullOrEmpty(save_path) == false)
+            {
+                AssetDatabase.ExportPackage(_path_holder.GetSelectedAssetPaths(), save_path);
+                // TODO: Open window to freshly created package.
+            }
+        }
+
+        private void SaveConfiguration()
+        {
+            if (string.IsNullOrEmpty(_save_name) == false)
+            {
+                PackageConfigurationSave save = ScriptableObject.CreateInstance<PackageConfigurationSave>();
+                string asset_path = _plugin_path;
+                if ( string.IsNullOrEmpty(asset_path) == true )
+                {
+                    asset_path = "Assets";
+                }
+                asset_path += "/PackageConfigurations";
+                if (AssetDatabase.IsValidFolder(asset_path) == false )
+                {
+                    AssetDatabase.CreateFolder(Directory.GetParent(asset_path).ToString(), "PackageConfigurations");
+                }
+
+                AssetDatabase.CreateAsset(save, asset_path + "/" + _save_name + ".asset");
+                save.SavePackageConfiguration(_path_holder);
+                AssetDatabase.SaveAssets();
+                RefreshContent();
+                SelectSaveByName(save.name);
+            }
+            else
+            {
+                Debug.LogWarning("Invalid file name: configuration has not been saved :S.");
+            }
+        }
+
+        private void SelectSaveByName(string name)
+        {
+            for ( int i = 0; i < _saves.Count; ++i )
+            {
+                if ( _saves[i].name.ToLower() == name.ToLower() )
+                {
+                    SelectSaveByID(i);
+                    return;
+                }
+            }
+        }
+
+        private void SelectSaveByID(int id)
+        {
+            _selected_save_id = id;
+            _path_holder.LoadSave(_saves[_selected_save_id]);
+            _save_name = _saves[_selected_save_id].name;
         }
 
         private void OnGUI()
         {
             using (var verticalScope = new GUILayout.VerticalScope("box"))
             {
-                if (_save_names.Count != 0)
+                if (_save_names != null && _save_names.Count != 0)
                 {
                     int new_selected_save_id = EditorGUILayout.Popup(_selected_save_id, _save_names.ToArray());
                     if ( new_selected_save_id != _selected_save_id )
                     {
-                        _selected_save_id = new_selected_save_id;
-                        _path_holder.LoadSave(_saves[_selected_save_id]);
-                        _save_name = _saves[_selected_save_id].name;
+                        SelectSaveByID(new_selected_save_id);
                     }
                 }
 
                 using (var horizontalScope = new GUILayout.HorizontalScope())
                 {
-                    _save_name = GUILayout.TextField(_save_name);
+                    GUILayout.Label("File name: ", GUILayout.Width(65));
+                    _save_name = GUILayout.TextField(_save_name, GUILayout.ExpandWidth(true));
                     if ( string.IsNullOrEmpty(_save_name) == false && GUILayout.Button("save", GUILayout.Width(80)) )
                     {
-                        ExportPackage();
+                        SaveConfiguration();
                     }
                 }
             }
@@ -78,12 +147,7 @@ namespace MM.PackageExporter
             }
             if (GUILayout.Button("EXPORT PACKAGE") == true)
             {
-                string save_path = EditorUtility.SaveFilePanel("Export package", "", "", "unitypackage");
-                if (string.IsNullOrEmpty(save_path) == false)
-                {
-                    AssetDatabase.ExportPackage(_path_holder.GetSelectedAssetPaths(), save_path);
-                    // TODO: Open window to freshly created package.
-                }
+                ExportPackage();
             }
         }
 
